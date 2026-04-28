a1fa99e987f3471705b10b7f405a9418df0eadd0
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-04-28 12:22:56 -0500

    feat: Build static card layout

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+export default function Card() {
+    return (
+        <div className="card-wrapper">
+            <div className="image-container">
+                <img src="src/assets/dummy-image.png"></img>
+            </div>
+            <div className="name-container">
+                <p>Mudkip</p>
+            </div>
+        </div>
+    )
+}
