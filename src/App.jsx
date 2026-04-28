c1c9041e5ae9f75ae2ca7ff9462299149314d9f3
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-04-28 15:45:11 -0500

    feat: Create api fetch request and pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { useState, useEffect, useRef } from "react"
 import GameBoard from "./components/Gameboard.jsx"
 
 export default function MainContent() {
+  const [allPokemonInfo, setAllPokemonInfo] = useState([]);
+  const hasFetched = useRef(false);
+
+  useEffect(() => { // API request for pokemon information
+    const fetchData = async () => {
+        const baseUrl = "https://pokeapi.co/api/v2/pokemon/"
+        const pokemonList = ["mudkip", "pikachu", "dachsbun", "volcarona", "orthworm", "sceptile", "bidoof", "piplup", "charizard", "gardevoir"]
+
+        try {
+
+          if (hasFetched.current === false) { // Guard for duplicate API request in strict mode
+            hasFetched.current = true;
+          } else return;
+          
+          let pokemonInfo = pokemonList.map((pokemon) => (
+              fetch(`${baseUrl}${pokemon}`).then((res) => res.json())
+          ));
+
+          const result = await Promise.all(pokemonInfo);
+            
+          const formattedPokemonInfo = result.map((pokemon) => ({
+            name: pokemon.name,
+            image: pokemon.sprites.other["official-artwork"].front_default
+          }));
+
+          setAllPokemonInfo(formattedPokemonInfo);
+
+        } catch(error) {
+            console.log(error);
+        }
+    };
+
+    fetchData();
+}, [])
+
   return (
     <div>
       <header className="header-wrapper">
@@ -14,7 +50,7 @@ export default function MainContent() {
         </article>
       </header>
       <main>
-        <GameBoard />
+        <GameBoard pokemonInfo={allPokemonInfo}/>
       </main>
     </div>
   )
